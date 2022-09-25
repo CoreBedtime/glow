fbdc728ee43409ea976c9bdb65fdfaed465978cc
Bedtime <kenkuku97@gmail.com>
2022-09-25 08:04:41 -0400

    Init

---
+++ glowapp/glowappApp.swift
@@ -0,0 +1,17 @@
+//
+//  glowappApp.swift
+//  glowapp
+//
+//  Created by bedtime on 9/25/22.
+//
+
+import SwiftUI
+
+@main
+struct glowappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
