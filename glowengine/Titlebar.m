a55423ad95997466224b8a7d36255d5f97e435f0
Bedtime <kenkuku97@gmail.com>
2022-09-29 15:01:53 -0400

    Structure

---
+++ glowengine/Titlebar.m
@@ -0,0 +1,53 @@
+//
+//  Titlebar.m
+//  glowengine
+//
+//  Created by bedtime on 9/26/22.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "ZKSwizzle.h"
+
+/*
+    This file adds titlebar graphics. 
+*/
+
+BOOL drawHovered = NO;
+
+hook(_NSThemeWidget)
+    -(void)viewDidMoveToWindow
+    {
+        ZKOrig(void);
+        [(NSView *)self addTrackingRect:[(NSView *)self bounds] owner:(NSView *)self userData:nil assumeInside:NO];
+    }
+
+    -(void)mouseEntered:(NSEvent *)theEvent
+    {
+        drawHovered = YES;
+    }
+
+    - (void)mouseExited:(NSEvent *)theEvent
+    {
+        drawHovered = NO;
+    }
+
+    -(void)mouseMoved:(NSEvent *)theEvent
+    {
+        drawHovered = YES;
+    }
+
+    -(void)drawRect:(CGRect)arg1
+    {
+        if (drawHovered)
+        {
+            [[[NSImage alloc] initWithContentsOfFile: [NSString stringWithFormat:@"/Library/Glow/%@_On.png", self.className]] drawInRect:arg1];
+        } else
+        {
+            [[[NSImage alloc] initWithContentsOfFile: [NSString stringWithFormat:@"/Library/Glow/%@_Off.png", self.className]] drawInRect:arg1];
+        }
+    }
+endhook
+
+/*
+   This here adds titlebar graphics. (Other apps.)
+*/
---
+++ glowengine/Window.m
@@ -0,0 +1,53 @@
+//
+//  Tweak.m
+//  glow
+//
+//  Created by bedtime on 7/1/22.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "ZKSwizzle.h"
+
+#define WindowMask @"WindowFrame_WindowShapeEdges_Regular.psd"
+#define WindowMaskSmall @"WindowFrame_WindowShapeEdges_Small.psd"
+
+@interface NSObject (Window)
+    -(id)displayName;
+@end
+
+@interface NSView (Private)
+    -(void)_lp_setCornerRadius:(double)arg1 continuous:(BOOL)arg2;
+@end
+
+
+/*
+    This here makes all windows square.
+*/
+hook(CUIThemeFacet)
+    -(struct CGImage *)copyLayerImageContentsAndCenter:(struct CGRect *)arg1 renditionKey:(id)arg2
+    {
+        if ([[self displayName] isEqual: WindowMask] || [[self displayName] isEqual: WindowMaskSmall])
+        {
+            NSImage *image = [[NSImage alloc] initWithSize:CGSizeMake(23, 23)];
+            [image lockFocus];
+            [NSColor.blackColor drawSwatchInRect:NSMakeRect(0, 0, 23, 23)];
+            [image unlockFocus];
+            CGImageSourceRef source;
+            source = CGImageSourceCreateWithData((CFDataRef)[image TIFFRepresentation], NULL);
+            return CGImageSourceCreateImageAtIndex(source, 0, NULL);
+        } else
+        {
+            return ZKOrig(struct CGImage *, arg1, arg2);
+        }
+    }
+endhook
+
+/*
+    This here fixes titlebars, due to the window mask being weird.
+*/
+hook(_NSTitlebarDecorationView)
+    -(void)updateLayer
+    {
+        [(NSView *)self removeFromSuperview];
+    }
+endhook
