fad3caab6a7568d25672dfe6d98c08520640e3b7
bedtime <70181219+i-pwl@users.noreply.github.com>
2022-12-08 21:54:31 -0500

    purge

--- glowengine/Window.m
+++ glowengine/Window.m
@@ -9,61 +9,35 @@
 #import "Utility/ZKSwizzle.h"
 #import "Utility/Preference.h"
 
-#define WindowMask @"WindowFrame_WindowShapeEdges_Regular.psd"
-#define WindowMask2 @"WindowFrame_WindowShapeEdges_Regular@2x.psd"
-
-@interface CUIThemeFacet : NSObject;
-    -(id)displayName;
+@interface CUIThemeRendition : NSObject
+    -(id)name;
+    -(id)data;
+    @property (nonatomic, readonly) NSData *srcData;
 @end
 
-@interface NSView (Private)
-    -(void)setCornerRadius:(double)arg1;
+@interface _CUIRawDataRendition : CUIThemeRendition
 @end
 
-
 /*
-    This here makes all windows square.
+    Hook coreUI.
 */
-hook(CUIThemeFacet)
-    -(struct CGImage *)copyLayerImageContentsAndCenter:(struct CGRect *)arg1 renditionKey:(id)arg2
+hook(_CUIRawDataRendition)
+    -(id)data
     {
-        if ([[(CUIThemeFacet *)self displayName] isEqual: WindowMask] || [[(CUIThemeFacet *)self displayName] isEqual: WindowMask2])
-        {
-            NSImage *image = [[NSImage alloc] initWithSize:CGSizeMake(23, 23)];
-            [image lockFocus];
-            [NSColor.blackColor drawSwatchInRect:NSMakeRect(0, 0, 23, 23)];
-            [image unlockFocus];
-            CGImageSourceRef source;
-            source = CGImageSourceCreateWithData((CFDataRef)[image TIFFRepresentation], NULL);
-            return CGImageSourceCreateImageAtIndex(source, 0, NULL);
-        } else
-        {
-            return ZKOrig(struct CGImage *, arg1, arg2);
-        }
-    }
-endhook
-
-/*
-    This here fixes titlebars, due to the window mask being weird.
-*/
-hook(_NSTitlebarDecorationView)
-    -(void)updateLayer
-    {
-        [(NSView *)self removeFromSuperview];
-    }
-endhook
-
-hook(NSWindow)
-    -(BOOL)_cornerMaskShouldDefineShadow
-    {
-        return NO;
-    }
-
-    -(void)update
-    {
-        ZKOrig(void);
+        NSDictionary *unarchived = ((NSMutableDictionary *)[NSKeyedUnarchiver unarchiveObjectWithData: ZKOrig(NSData *)]);
+        CALayer *layer;
         
-        [(NSView *)((NSWindow *)self).contentView.superview setCornerRadius: [Preference getFloat:"WindowRadius"]];
-        [((NSWindow *)self) invalidateShadow];
+        if ([unarchived isKindOfClass:[NSDictionary class]])
+        {
+            if ([[unarchived objectForKey:@"rootLayer"] isKindOfClass:[CALayer class]])
+            {
+                //layer = [unarchived objectForKey:@"rootLayer"];
+                layer = [CALayer new];
+                //layer.backgroundColor = [NSColor redColor].CGColor;
+                layer.contents = [[NSImage alloc] initWithContentsOfFile:@"/Library/Glow/Titlebar.png"];
+            }
+        }
+        
+        return [NSKeyedArchiver archivedDataWithRootObject:@{@"rootLayer" : layer}];
     }
 endhook
